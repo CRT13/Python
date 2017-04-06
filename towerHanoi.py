bc53ba14adc9876cf12e9677b89b4ff61bf30558
Chaitanya Tejaswi <aeronfinium@gmail.com>
2017-04-06 22:42:01 +0530

    Create towerHanoi.py

---
+++ towerHanoi.py
@@ -0,0 +1,14 @@
+#Algorithm - TowerOfHanoi
+def moveTower(height,fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1,fromPole,withPole,toPole)
+        moveDisk(fromPole,toPole)
+        moveTower(height-1,withPole,toPole,fromPole)
+def moveDisk(fp,tp):
+    print("moving disk from",fp,"to",tp)
+
+#Implementation
+print('======================================')
+height = int(input("Number of disks? "))
+print('======================================')
+moveTower(height,"A","B","C")
